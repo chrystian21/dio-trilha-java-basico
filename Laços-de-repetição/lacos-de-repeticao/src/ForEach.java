e8304bd026a66cc765d6a0f20e39493f7850bda0
Chrystian <chrystiancarlosdasilva@gmail.com>
2024-05-14 17:01:27 -0300

    "Estruturas de repetição"

---
+++ Laços-de-repetição/lacos-de-repeticao/src/ForEach.java
@@ -0,0 +1,9 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String alunos[] = {"Felipe","Jonas","Julia","Marcos"};
+
+        for(String aluno : alunos){
+            System.out.println(aluno);
+        }
+    }
+}
