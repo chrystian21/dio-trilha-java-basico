6396c9051981985c2de9bd736ba84879c25c40ff
Chrystian <chrystiancarlosdasilva@gmail.com>
2024-05-14 12:45:25 -0300

    "Atividades de controle de fluxo"

---
+++ Estruturas-condicionais/Estrutura-Condicional/src/CondicaoTernaria.java
@@ -0,0 +1,8 @@
+public class CondicaoTernaria {
+    public static void main(String[] args) {
+        int nota = 7;
+        String resultado = nota >=7 ?  "aprovado" : "Reprovado" ;
+
+        System.out.println(resultado);
+    }
+}
